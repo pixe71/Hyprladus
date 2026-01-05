a72e37e9fadd84ff627b3576735d7f416278e396
Luc Tournie <luc.tourniem@gmail.com>
2026-01-05 19:50:27 +0100

    Fix: ignore cache files and clean git status

--- cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#c4c3ca", "#13122b" },
-	[SchemeSel] = { "#c4c3ca", "#0662B0" },
-	[SchemeOut] = { "#c4c3ca", "#42A4E1" },
-};
