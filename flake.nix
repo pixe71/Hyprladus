04b8b08fd9d72b2b365801a3b792840be27058c2
Luc Tournie <luc.tourniem@gmail.com>
2026-01-07 21:04:12 +0100

    Fix bug

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs = "follows nixpkgs";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     hyprland.url = "github:hyprwm/Hyprland";
   };
 
@@ -13,14 +13,15 @@
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
-        ./nixos/configuration.nix
+        ./configuration.nix
+        
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           
-          # Replace 'user' with your actual username found in configuration.nix and home.nix
-          home-manager.users.user = import ./nixos/home.nix;
+          # Si tu n'as pas encore créé de user et que tu es root, commente cette ligne pour l'instant.
+          home-manager.users.root = import ./home.nix;
           
           home-manager.extraSpecialArgs = { inherit inputs; };
         }
